2cbea9b2e25d99d55ecd273a53ba66a6e46ddfc3
HassenYahia <hassenabdurahman4047@gmail.com>
2023-10-07 07:06:00 -0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Hassen Abdurahman Yahia on 2023-10-07.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
